1858f5ecf4b0d50a0918c46d75d871f960025403
bbulliard <bbulliard@gmail.com>
2022-02-27 12:20:50 -0600

    Initial Commit

---
+++ send_messagew/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  send_messagew
+//
+//  Created by Bob Bulliard on 2/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ send_messagew/send_messagewApp.swift
@@ -0,0 +1,17 @@
+//
+//  send_messagewApp.swift
+//  send_messagew
+//
+//  Created by Bob Bulliard on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct send_messagewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
