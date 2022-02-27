bdf7c0327987716d5ed13a0619080c866afdc89f
bbulliard <bbulliard@gmail.com>
2022-02-27 13:05:27 -0600

    20220227_1300_works

--- send_messagew/ContentView.swift
+++ send_messagew/ContentView.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
-
+import Messages
 struct ContentView: View {
+    @State var numbertomessage = ""
+    @State var message = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+  
+        TextField("Enter a Mobile num:(1xxxxxxxxxx", text: $numbertomessage)
+        TextField("Enter a message", text: $message)
+        
+        Button("send") {
+            sendmessage(parm1: numbertomessage, parm2: message)
+        //av4(parm1: amtx6, parm2: amtx7, parm3: amtx8)
+        }
+
+        
     }
+}
+
+func sendmessage(parm1: String, parm2: String) {
+    //func av4(parm1: Double, parm2: Double, parm3: Double) -> Double {
+    let sms: String = "sms:\(parm1)&body=\(parm2)"
+    let strurl: String =  sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+    UIApplication.shared.open(URL.init(string:strurl)!, options: [:], completionHandler: nil)
 }
 
 struct ContentView_Previews: PreviewProvider {
